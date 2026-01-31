bffad7ecbd13c64a49a5e094be587fab4415beb3
Your Name <dkocausta34@gmail.com>
2026-01-31 13:32:41 -0500

    Tetst: Verify lint-staged/husky setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
+
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
@@ -29,7 +30,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private configService: ConfigService) {}
+  get dbUrl(): string {
+    // Will throw at app start if not validated by Joi above
+    return this.configService.get<string>('DATABASE_URL');
   }
 }
